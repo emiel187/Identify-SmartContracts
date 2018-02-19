bd0d6cd57a74b7264eb95a3cd360c5611c62fc07
Kevin Leyssens <kevinleyssens@hotmail.com>
2018-02-19 11:31:29 +0100

    CustomToken

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,6 @@
 var Identify = artifacts.require('./token/Identify.sol');
 
 module.exports = function(deployer, network) {
-  deployer.deploy(Identify,"Identify Token", "IDF", 6);
+  deployer.deploy(Identify);
 
 }
