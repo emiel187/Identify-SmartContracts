c045a318731bad2c9a316cd2249119ddcad4a957
Jeroen De Prest <jeroen.deprest@hotmail.com>
2018-02-26 16:53:54 +0100

    Added more tests

--- contracts/crowdsale/Whitelist.sol
+++ contracts/crowdsale/Whitelist.sol
@@ -11,10 +11,13 @@ contract Whitelist is Ownable {
     bool public paused = false;
 
     uint256 public participantAmount;
+    uint256 private adminAmount;
 
-    mapping (address => bool) private participants;
+    mapping (address => bool) private isParticipant;
+    mapping (address => bool) public isAdmin;
 
     event AddParticipant(address _participant);
+    event RemoveParticipant(address _participant);
     event Paused(address _owner, uint256 _time);
     event Resumed(address _owner, uint256 _time);
     event Finalized(address _owner, uint256 _time);
@@ -29,44 +32,85 @@ contract Whitelist is Ownable {
         _;
     }
 
+    modifier onlyAdmin() {
+        require(isAdmin[msg.sender]);
+        _;
+    }
+
     function () payable public {
-        addParticipant(msg.sender);
         // give ETH back
         msg.sender.transfer(msg.value);
     }
 
     function Whitelist() public {
+        addAdmin(msg.sender);
     }
 
     function addSelfAsParticipant() public {
         require(addParticipant(msg.sender));
     }
 
-    function addParticipant(address _participant) public notPaused notFinalized returns (bool) {
+    function addParticipant(address _participant) public notPaused onlyAdmin notFinalized returns (bool) {
         require(address(_participant) != 0);
-        require(participants[_participant] == false);
+        require(isParticipant[_participant] == false);
 
 
-        participants[_participant] = true;
+        isParticipant[_participant] = true;
         participantAmount++;
         AddParticipant(_participant);
         return true;
     }
 
-    // Needs to be reworked
-    // Function used to save gas.
-    function addFiveParticipants(address _participantOne, address _participantTwo, address _participantThree, address _participantFour, address _participantFive) public onlyOwner notFinalized returns (bool) {
-        
-        require(addParticipant(_participantOne));
-        require(addParticipant(_participantTwo));
-        require(addParticipant(_participantThree));
-        require(addParticipant(_participantFour));
-        require(addParticipant(_participantFive));
+    function removeParticipant(address _participant) public notPaused onlyAdmin notFinalized returns (bool) {
+        require(address(_participant) != 0);
+        require(isParticipant[_participant] == true);
+        require(msg.sender != _participant);
+
+
+        delete isParticipant[_participant];
+        participantAmount--;
+        RemoveParticipant(_participant);
         return true;
     }
 
+    function addAdmin(address _admin) public notPaused onlyOwner notFinalized returns (bool) {
+        require(address(_admin) != 0);
+        require(!isAdmin[_admin]);
+
+
+        isAdmin[_admin] = true;
+        //participantAmount++;
+        // AddAdmin(_admin);
+        return true;
+    }
+
+    function removeAdmin(address _admin) public notPaused onlyOwner notFinalized returns (bool) {
+        require(address(_admin) != 0);
+        require(isAdmin[_admin] == true);
+        require(msg.sender != _admin);
+
+        delete isAdmin[_admin];
+        return true;
+    }
+
+    // // Needs to be reworked
+    // // Function used to save gas.
+    // function addFiveParticipants(address _participantOne, address _participantTwo, address _participantThree, address _participantFour, address _participantFive) public onlyOwner notFinalized returns (bool) {
+        
+    //     require(addParticipant(_participantOne));
+    //     require(addParticipant(_participantTwo));
+    //     require(addParticipant(_participantThree));
+    //     require(addParticipant(_participantFour));
+    //     require(addParticipant(_participantFive));
+    //     return true;
+    // }
+
     function getTotalParticipants() public view returns (uint256) {
         return participantAmount;
+    }
+
+    function getTotalAdmins() public view onlyAdmin returns (uint256) {
+        return adminAmount;
     }
 
     // @notice Pauses the whitelist if there is any issue
--- test/Whitelist.js
+++ test/Whitelist.js
@@ -8,7 +8,7 @@ contract('Whitelist', function(accounts) {
 
     var account_one = accounts[0];
     var account_two = accounts[1];
-    var account_empty = accounts[4];
+    var account_four = accounts[4];
 
     var whitelistCount_start;
 
@@ -22,7 +22,18 @@ contract('Whitelist', function(accounts) {
             whitelistCount_start = count.toNumber();
         });
     });
-    //TODO check if i need my version of getTier to make sure that address is in the mapping
+
+    it('Should be able to use the constructor', function(done) {
+        Whitelist.new().then(
+                 function(whitelist) {
+                    whitelist.isAdmin(account_one).then(
+                       function(isAdmin) {
+                          assert.equal(isAdmin, true, "Owner should be admin");
+                          done();
+                       }).catch(done);
+                 }).catch(done);
+        });
+
 
     // test added succesfull
     it("Should add self succesfull to list", function() {
@@ -53,6 +64,24 @@ contract('Whitelist', function(accounts) {
         return Whitelist.deployed().then(function(instance) {
             meta = instance;
             return meta.addParticipant("account_two");
+        }).then(function(){
+            inThen = true;
+            assert.ok(false,"Should have failed");
+        }).catch(function(err){
+            if(inThen){
+                assert.ok(false, "Should have failed");
+            } else {
+                assert.ok(true, "Failed succesfull");
+            }
+        });
+    });
+
+    it("Should not add other succesfull to list when not an admin", function() {
+        var inThen = false;
+
+        return Whitelist.deployed().then(function(instance) {
+            meta = instance;
+            return meta.addParticipant(accounts[6],{from:account_two, gas: 3000000});
         }).then(function(){
             inThen = true;
             assert.ok(false,"Should have failed");
@@ -114,22 +143,7 @@ contract('Whitelist', function(accounts) {
         });
     });
 
-    // // test tier non existing account
-    // it("Check tier of non existing account, should be 0", function() {
-        
-    //     return Whitelist.deployed().then(function(instance) {
-    //         meta = instance;
-    //         return meta.getTier(account_empty);
-    //     }).then(function(tier){
-    //         if(tier.toNumber() === 0){
-    //             return meta.getTier(account_empty);
-    //         } else {
-    //             return meta.getTier(accounts[9]);
-    //         }
-    //     }).then(function(tier){
-    //         assert.equal(tier.toNumber(),0, "Should be 0, because not in whitelist")
-    //     });
-    // });
+    
 
     // test that owner can stop
     it("Only owner can call stop method", function() {
@@ -151,39 +165,140 @@ contract('Whitelist', function(accounts) {
         ;
     });
 
+    it("Should not be able to add admins", function() {
+        var inThen;
 
+        return Whitelist.deployed().then(function(instance) {
+            meta = instance;
+            return meta.addAdmin(account_two,{from:account_two, gas: 3000000});
+        }).then(function(){
+            inThen = true;
+            assert.ok(false, "Should have failed with testrpc");     
+        }).catch(function(){
+            if (inThen) {
+                assert.ok(false, "should have failed directly");
+            } else {
+                assert.ok(true, "Failed because only owner can call stop method");
+            }
+        })
+        ;
+    });
 
-    // it("Should remove an existing account and update the count. After add this one again", function() {
-    //     var whitelistCount_inter;
+    it("Only owner can add to admins", function() {
+        var inThen;
+        return Whitelist.deployed().then(function(instance) {
+            meta = instance;
+            return meta.isAdmin(account_one);
+        }).then(function(isAdmin){
+            assert.equal(isAdmin, true, "Owner should be admin")
+            return meta.addAdmin(account_two,{from:account_one, gas: 3000000});
+        }).then(function(){
+            return meta.isAdmin(account_two);
+        }).then(function(isAdmin){
+            assert.equal(isAdmin, true, "New account should be admin");
+        })
+    });
 
-    //     return Whitelist.deployed().then(function(instance) {
-    //         meta = instance;
-    //         return meta.getTier(accounts[7]);
-    //     }).then(function(tier){
-    //         if(tier.toNumber() === 0){
-    //             return meta.addParticipant(accounts[7])
-    //         } else {
-    //             return true;
-    //         }
-    //     }).then(function(){
-    //         return meta.participantAmount.call();
-    //     }).then(function(count){
-    //         whitelistCount_inter = count.toNumber();
-    //         return meta.removeFromList(accounts[7]);
-    //     }).then(function(){
-    //         return meta.participantAmount.call();
-    //     }).then(function(count){
-    //         assert.equal(count.toNumber(),whitelistCount_inter - 1, "Should be one less");
-    //         return true;
-    //     }).then(function(){
-    //         return meta.addParticipant(accounts[7])
-    //     }).then(function(){
-    //         return meta.participantAmount.call();   
-    //     }).then(function(count){
-    //         assert.equal(count.toNumber(),whitelistCount_inter, "Should be the same as in the beginning");
-    //     })
-    //     ;
-    // });
+    it("Should remove admin", function() {
+        var inThen;
+        return Whitelist.deployed().then(function(instance) {
+            meta = instance;
+            return meta.addAdmin(accounts[5],{from:account_one, gas: 3000000});
+        }).then(function(){
+            return meta.isAdmin(accounts[5]);
+        }).then(function(isAdmin){
+            assert.equal(isAdmin, true, "New account should be admin");
+        }).then(function(){
+            return meta.removeAdmin(accounts[5],{from:account_one, gas: 3000000});
+        }).then(function(){
+            return meta.isAdmin(accounts[5]);
+        }).then(function(isAdmin){
+            assert.equal(isAdmin, false, "Account should not be admin anymore");
+        })
+    });
+
+    it("Should not be able to remove self from admin", function() {
+        var inThen;
+
+        return Whitelist.deployed().then(function(instance) {
+            meta = instance;
+            return meta.removeAdmin(account_one,{from:account_one, gas: 3000000});
+        }).then(function(){
+            inThen = true;
+            assert.ok(false, "Should have failed with testrpc");     
+        }).catch(function(){
+            if (inThen) {
+                assert.ok(false, "should have failed directly");
+            } else {
+                assert.ok(true, "Failed because only owner can call stop method");
+            }
+        })
+        ;
+    });
+
+    it("Should remove an existing account and update the count. After add this one again", function() {
+        var whitelistCount_inter;
+
+        return Whitelist.deployed().then(function(instance) {
+            meta = instance;
+        }).then(function(){
+            return meta.addParticipant(accounts[7])
+        }).then(function(){
+            return meta.participantAmount.call();
+        }).then(function(count){
+            whitelistCount_inter = count.toNumber();
+            return meta.removeParticipant(accounts[7]);
+        }).then(function(){
+            return meta.participantAmount.call();
+        }).then(function(count){
+            assert.equal(count.toNumber(),whitelistCount_inter - 1, "Should be one less");
+            return true;
+        }).then(function(){
+            return meta.addParticipant(accounts[7])
+        }).then(function(){
+            return meta.participantAmount.call();   
+        }).then(function(count){
+            assert.equal(count.toNumber(),whitelistCount_inter, "Should be the same as in the beginning");
+        })
+        ;
+    });
+
+    it("Should not remove self", function() {
+        var inThen;
+        
+        return Whitelist.deployed().then(function(instance) {
+            meta = instance;
+            return meta.removeParticipant(account_one,{from:account_one, gas: 3000000})
+        }).then(function(){
+            inThen = true;
+            assert.ok(false, "Should have failed with testrpc");            
+        }).catch(function(err){
+            if (inThen) {
+                assert.ok(false, "should have failed directly");
+            } else {
+                assert.ok(true, "Failed because list is paused");
+            }
+        });
+    });
+
+    it("Should not remove other from list when not an address", function() {
+        var inThen = false;
+
+        return Whitelist.deployed().then(function(instance) {
+            meta = instance;
+            return meta.removeParticipant("account_two");
+        }).then(function(){
+            inThen = true;
+            assert.ok(false,"Should have failed");
+        }).catch(function(err){
+            if(inThen){
+                assert.ok(false, "Should have failed");
+            } else {
+                assert.ok(true, "Failed succesfull");
+            }
+        });
+    });
+
 
     // test fallback function
     it("Should register with fallback function and transfer ether back", function() {
@@ -194,23 +309,20 @@ contract('Whitelist', function(accounts) {
             return web3.eth.getBalance(accounts[8]);            
         }).then(function(balance){
             account_eight_balance = balance.toNumber();
+            return true;
         }).then(function(){
             
             //fallback function
             return meta.sendTransaction({from: accounts[8], gas: 3000000, value: 1}); 
             
         })
-        .then(function(){
-            return meta.participantAmount.call();
-        }).then(function(count){
-            assert.equal(count.toNumber(),whitelistCount_start+1, "Should have added one to the count");
+        then(function(){
             return web3.eth.getBalance(accounts[8]);
         }).then(function(balance){
             // need to take in account the gascosts. But won't be over 0.5 ETH
             assert.ok(balance.toNumber() > (account_eight_balance - web3.toWei(0.5, "ether")),"Should have returned the amount");
         });
     });
-
 
     it("Should not register when stopped", function() {
         var inThen;
