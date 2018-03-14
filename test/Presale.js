dfdf8835c29c6c493a8a08782d1037d50ea11b66
Kevin Leyssens <kevinleyssens@hotmail.com>
2018-03-14 16:40:45 +0100

    presale tests working

--- test/Presale.js
+++ test/Presale.js
@@ -172,9 +172,7 @@ contract('Presale', function (accounts) {
     it('Should not transferownershiptoken when invoking as owner', function () {
         var inThen;
 
-        return metaWhitelist.addParticipant(accounts[9], { from: account_one, gas: 3000000 }).then(() => {
-            return metaPresale.transferOwnershipToken(metaPresaleV2.address);
-        }).then(function () {
+        return metaPresale.transferOwnershipToken(metaPresaleV2.address).then(function () {
             inThen = true;
             assert.ok(false, "Should have failed");
         }).catch(function (err) {
@@ -188,7 +186,7 @@ contract('Presale', function (accounts) {
 
     // test transferownershiptoken as multisig
     it('Should transferownershiptoken when invoking as multisigwallet', function () {
-        
+
         var new_owner = metaPresaleV2.address.substring(2, (metaPresaleV2.address.length));
         // transferownership token
         var function_data = "0x9ae6892b";
@@ -327,13 +325,52 @@ contract('Presale', function (accounts) {
         });
     });
 
-    
 
     // test transferownership as owner and not as multisig
+    it('Should not transfer ownership as owner', function () {
+        var inThen = false;
+
+        return Presale.deployed().then(function (instance) {
+            metaPresale = instance;
+            return metaPresale.owner.call();
+        }).then(function (owner) {
+            assert.equal(owner, account_one);
+            return metaPresale.transferOwnership(account_two, { from: account_one });
+        }).then(function () {
+            inThen = true;
+            assert.ok(false, "Should have failed");
+        }).catch(function (err) {
+            if (inThen) {
+                assert.ok(false, "Should have failed");
+            } else {
+                assert.ok(true, "Failed succesfull");
+            }
+        });
+    });
+
 
     // test transferownership as multisig
 
-
-
+    it('Should transferownership when invoking as multisigwallet', function () {
+        
+                var new_owner = account_two.substring(2, (account_two.length));
+                // transferownership
+                var function_data = "0xf2fde38b";
+                var data = function_data + "000000000000000000000000" + new_owner;
+        
+                return MultiSigWallet.deployed().then(function (instance) {
+                    metaMultisig = instance;
+                    return metaMultisig.submitTransaction(Presale.address, 0, data, { from: account_one, gas: 3000000 });
+                }).then(function () {
+                    return metaMultisig.transactionCount.call();
+                }).then(function (transactioncount) {
+                    var transaction_id = (transactioncount.toNumber() - 1);
+                    return metaMultisig.confirmTransaction(transaction_id, { from: account_two, gas: 3000000 });
+                }).then(function () {
+                    return metaPresale.owner.call();
+                }).then(function (owner) {
+                    assert.equal(owner, account_two, "Should transfered successful");
+                });
+            });
 
 });
